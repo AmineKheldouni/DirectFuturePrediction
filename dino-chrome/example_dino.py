eae852c3686f6a4414dfb8e3c7d43f502336eea0
AmineKheldouni <aminekheldouni@gmail.com>
2018-12-24 11:55:52 +0100

    Added vizdoom binfile and docs for the Object Recognition Project.

---
+++ dino-chrome/example_dino.py
@@ -0,0 +1,5 @@
+'''
+Environment: https://chromedino.com/
+explanations: https://medium.com/acing-ai/how-i-build-an-ai-to-play-dino-run-e37f37bdf153
+cf. https://github.com/Paperspace/DinoRunTutorial
+'''
