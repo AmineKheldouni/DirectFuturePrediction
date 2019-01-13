0e01b56c1c8ce7b64ecdf4fa4efd980111cf4c12
AmineKheldouni <aminekheldouni@gmail.com>
2019-01-13 15:57:07 +0100

    Clean

--- tmp/dino-chrome/example_dino.py
+++
@@ -1,5 +0,0 @@
-'''
-Environment: https://chromedino.com/
-explanations: https://medium.com/acing-ai/how-i-build-an-ai-to-play-dino-run-e37f37bdf153
-cf. https://github.com/Paperspace/DinoRunTutorial
-'''
