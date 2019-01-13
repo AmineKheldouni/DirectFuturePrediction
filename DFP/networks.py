5d57f5df899c6e11c32475bc10c3179f2a8ea3d4
AmineKheldouni <aminekheldouni@gmail.com>
2019-01-13 18:10:44 +0100

    Correct the network expectation/action stream

--- DFP/networks.py
+++ DFP/networks.py
@@ -24,53 +24,6 @@ from keras import backend as K
 import tensorflow as tf
 #tf.python.control_flow_ops = tf
 
-
-class Average(Merge):
-    """Layer that averages a list of inputs.
-    It takes as input a list of tensors,
-    all of the same shape, and returns
-    a single tensor (also of the same shape).
-    """
-
-    def _merge_function(self, inputs):
-        output = inputs[0]
-        for i in range(1, len(inputs)):
-            output += inputs[i]
-        return output / len(inputs)
-
-class Subtract(Merge):
-    """Layer that subtracts two inputs.
-    It takes as input a list of tensors of size 2,
-    both of the same shape, and returns a single tensor, (inputs[0] - inputs[1]),
-    also of the same shape.
-    # Examples
-    ```python
-        import keras
-        input1 = keras.layers.Input(shape=(16,))
-        x1 = keras.layers.Dense(8, activation='relu')(input1)
-        input2 = keras.layers.Input(shape=(32,))
-        x2 = keras.layers.Dense(8, activation='relu')(input2)
-        # Equivalent to subtracted = keras.layers.subtract([x1, x2])
-        subtracted = keras.layers.Subtract()([x1, x2])
-        out = keras.layers.Dense(4)(subtracted)
-        model = keras.models.Model(inputs=[input1, input2], outputs=out)
-    ```
-    """
-
-    def build(self, input_shape):
-        super(Subtract, self).build(input_shape)
-        if len(input_shape) != 2:
-            raise ValueError('A `Subtract` layer should be called '
-                             'on exactly 2 inputs')
-
-    def _merge_function(self, inputs):
-        if len(inputs) != 2:
-            raise ValueError('A `Subtract` layer should be called '
-                             'on exactly 2 inputs')
-        return inputs[0] - inputs[1]
-
-
-
 class Networks(object):
 
     @staticmethod
@@ -105,16 +58,11 @@ class Networks(object):
 
         expectation_stream = Dense(measurement_pred_size, activation='relu')(concat_feat)
 
-        action_stream = []
-        for i in range(action_size):
-            action_stream.append(Dense(measurement_pred_size, activation='relu')(concat_feat))
-
-        avg = Average()(action_stream)
-
         prediction_list = []
         for i in range(action_size):
-            tmp = Subtract()([action_stream[i], avg])
-            prediction_list.append(merge([tmp, expectation_stream], mode='sum'))
+            action_stream = Dense(measurement_pred_size, activation='relu')(concat_feat)
+            # prediction_list.append(action_stream)
+            prediction_list.append(merge([action_stream, expectation_stream], mode='sum'))
 
         model = Model(input=[state_input, measurement_input, goal_input], output=prediction_list)
 
