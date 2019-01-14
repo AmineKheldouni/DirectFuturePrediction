c9bd04f32b3fe8b7b4272d9638d6fece7e33a278
AmineKheldouni <aminekheldouni@gmail.com>
2019-01-14 01:45:26 +0100

    Segmentation module plugged to DFP model but too slow.

--- DFP/segmentation_image.py
+++ DFP/segmentation_image.py
@@ -1,55 +1,5 @@
-import os
-import sys
-import random
-import math
 import numpy as np
-import skimage.io
-import matplotlib
-import matplotlib.pyplot as plt
 
-# Root directory of the project
-ROOT_DIR = os.path.abspath("../../Mask_RCNN_matterport/")
-
-# Import Mask RCNN
-sys.path.append(ROOT_DIR)  # To find local version of the library
-from mrcnn import utils
-import mrcnn.model as modellib
-from mrcnn import visualize
-# Import COCO config
-sys.path.append(os.path.join(ROOT_DIR, "samples/coco/"))  # To find local version
-import coco
-
-# Directory to save logs and trained model
-MODEL_DIR = os.path.join(ROOT_DIR, "logs")
-
-# Local path to trained weights file
-COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
-# Download COCO trained weights from Releases if needed
-if not os.path.exists(COCO_MODEL_PATH):
-    utils.download_trained_weights(COCO_MODEL_PATH)
-
-# Directory of images to run detection on
-IMAGE_DIR = os.path.join(ROOT_DIR, "images")
-
-class InferenceConfig(coco.CocoConfig):
-    # Set batch size to 1 since we'll be running inference on
-    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
-    GPU_COUNT = 1
-    IMAGES_PER_GPU = 1
-
-config = InferenceConfig()
-config.display()
-
-# Create model object in inference mode.
-model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
-
-# Load weights trained on MS-COCO
-model.load_weights(COCO_MODEL_PATH, by_name=True)
-
-
-# COCO Class names
-# Index of the class in the list is its ID. For example, to get ID of
-# the teddy bear class, use: CLASS_NAMES.index('teddy bear')
 CLASS_NAMES = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
                'bus', 'train', 'truck', 'boat', 'traffic light',
                'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
@@ -65,7 +15,6 @@ CLASS_NAMES = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
                'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
                'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
                'teddy bear', 'hair drier', 'toothbrush']
-
 
 def display_medkit_mask(image, mask, regions, class_ids, scores, CLASS_NAMES):
     """Display the given image and the top few class masks."""
@@ -87,7 +36,6 @@ def display_medkit_mask(image, mask, regions, class_ids, scores, CLASS_NAMES):
     # Pull masks of instances belonging to the same class.
     m = mask[:, :, np.where(class_ids == class_id)[0]].astype('float32')
     m = np.sum(m * np.arange(1, m.shape[-1] + 1), -1)
-    print(regions)
 
     for i in range(len(regions)):
         m[regions[i][0]:regions[i][2],regions[i][1]:regions[i][3]] = \
@@ -97,12 +45,12 @@ def display_medkit_mask(image, mask, regions, class_ids, scores, CLASS_NAMES):
     # visualize.display_images(to_display, titles=['simulator image', 'medkit mask'], cols= 2, cmap="Blues_r")
     return m
 
-def predict_segmentation(image):
-    results = model.detect([image], verbose=1)
+def predict_segmentation(image, model):
+    results = model.detect([image], verbose=0)
     # Visualize results
     r = results[0]
-    visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'],
-                                CLASS_NAMES, r['scores'])
+    # visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'],
+                                # CLASS_NAMES, r['scores'])
     m = display_medkit_mask(image, r['masks'], r['rois'], r['class_ids'], r['scores'], CLASS_NAMES)
 
     return m
